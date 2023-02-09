545464b9eaf961a3c72640cb14d9bc34b824e437
Sairam Madem <sairam_madem@vrtx.com>
2023-02-08 22:49:33 -0500

    first commit

---
+++ src/main/java/com/vrtx/tools/sonargithubdemo/SonarGithubDemoApplication.java
@@ -0,0 +1,11 @@
+package com.vrtx.tools.sonargithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarGithubDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SonarGithubDemoApplication.class, args);
+	}
+}
